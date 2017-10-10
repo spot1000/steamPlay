3bf33c177a0c7f8361fe89a9d72cf15b3b379026
DESKTOP-KMFNE6L\Adam <adam.lichter@radient360.com>
2017-10-09 22:56:26 -0230

    added env, and got all the promises working

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var axios = require('axios')
 
+
 var app = express();
 
 // view engine setup
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ let express = require('express');
 let router = express.Router();
 let axios = require('axios');
 
-let apiKey = "DDDD16E842E75C868F017A3A541C69D4";
+require('dotenv').config()
+
+let apiKey = process.env.APIKEY;
+console.log(process.env.APIKEY)
 let httpAddress = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + apiKey
 
 let returnarray = []
 
-console.log(apiKey);
 router.get('/:args', (req, res, next) => {
   let mypromises = [];
   let array = req.params.args.split('&');
@@ -17,24 +19,27 @@ router.get('/:args', (req, res, next) => {
     mypromises.push(axios.get(httpAddress + "&steamid=" + x + "&format=json"))
   })
   console.log(mypromises)
-  axios.all(mypromises)
-    .then(axios.spread((...args) => {// figure this part out
-      console.log(args.data)
-    }
-  ))
+  axios.all([axios.get(httpAddress + "&steamid=" + array[0] + "&format=json"), axios.get(httpAddress + "&steamid=" + array[1] + "&format=json")])
+    .then( (results) => {
+      let temp = results.map(r => r.data)
+      console.log(temp);
+      let count = temp.map(count => count.response.game_count)
+      console.log(count.indexOf(Math.min(count)))
+      res.send(count.indexOf(Math.min(count)))
+
+    })
 
 
 
 
 
   // axios.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + apiKey + '&steamid=76561198010164774&format=json')
-  //   .then(response => {
+  //   .then((response) => {
+  //     console.log(response.data); // ex.: { user: 'Your User'}
+  //
   //
   //     res.send(response.data);
   //   });
-  //
-  //   console.log(response.data); // ex.: { user: 'Your User'}
-  //   console.log(response.status); // ex.: 200
   });
 
 /* GET home page. */
